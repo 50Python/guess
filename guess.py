4bb85d2b4cb27a54a352732d0ce720075b1cd831
50Python <C931004@mail4u.tun.edu.tw>
2023-10-31 14:37:58 +0800

    update code

--- guess.py
+++ guess.py
@@ -6,12 +6,11 @@ times = 0
 while True:
 	check = input('猜猜看現在數字是多少: ')
 	check = int(check)
-	times = times + 1
+	times = times + 1 #可以寫成 times += 1 
 	print('到目前已經猜了', times  , '次')
 	if check < password:
 		print('猜得比答案小')
 		print('提示:' , password) #測試用
-		#print('到目前已經猜了',  , '次')
 	elif check > password:
 		print('猜得比答案大')	
 		print('提示:' , password) #測試用
